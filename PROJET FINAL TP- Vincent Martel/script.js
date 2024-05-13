5a9cff247ba03cc26c459e44e309b15020935611
mcvince88 <vincentmartel88@gmail.com>
2024-05-13 19:39:15 -0400

    Add files via upload

---
+++ PROJET FINAL TP- Vincent Martel/script.js
@@ -0,0 +1,25 @@
+function onePiece() {
+    accueil.style.backgroundImage = `url(one-piece-crew.webp)`;
+    accueil.style.backgroundRepeat = `repeat`;
+    accueil.style.backgroundSize = `cover`;
+    accueil.style.color = "aqua";
+    accueil.style.textShadow = "1px 1px 2px black, 0 0 25px blue, 0 0 5px darkblue";
+    
+}
+function nuit() {
+    accueil.style.backgroundImage = `radial-gradient(closest-side, transparent 0%, transparent 75%, #666a59 76%, #d0d1cd 85%, #72796c 86%, #666863 94%, #000000 95%, #898686 103%, #bec4a7 104%, #404336 112%, #3b3e30 113%, #454d2a 121%, #000000 122%, #000000 130%, #3f4836 131%, #60645e 140%), 
+        radial-gradient(closest-side, transparent 10%, transparent 75%, #41433a 76%, #8e9183 85%, #e0e6da 86%, #EDFFDB 94%, #000000 95%, #000000 103%, #626752 104%, #D9E6A7 112%, #393c2d 113%, #3e4622 121%, #000000 122%, #000000 130%, #686f62 131%, #E0EAD7 140%)`;
+    accueil.style.color = "#e8e123";
+    accueil.style.textShadow = "1px 1px 2px black, 0 0 25px blue, 0 0 5px darkblue";
+    accueil.style.backgroundRepeat = `repeat`;
+    accueil.style.backgroundSize = "110px 110px";
+}
+function jour() {
+    accueil.style.backgroundImage = `radial-gradient(closest-side, transparent 0%, transparent 75%, #B6CC66 76%, #B6CC66 85%, #EDFFDB 86%, #EDFFDB 94%, #FFFFFF 95%, #FFFFFF 103%, #D9E6A7 104%, #D9E6A7 112%, #798B3C 113%, #798B3C 121%, #FFFFFF 122%, #FFFFFF 130%, #E0EAD7 131%, #E0EAD7 140%),
+radial-gradient(closest-side, transparent 0%, transparent 75%, #B6CC66 76%, #B6CC66 85%, #EDFFDB 86%, #EDFFDB 94%, #FFFFFF 95%, #FFFFFF 103%, #D9E6A7 104%, #D9E6A7 112%, #798B3C 113%, #798B3C 121%, #FFFFFF 122%, #FFFFFF 130%, #E0EAD7 131%, #E0EAD7 140%)`;
+    accueil.style.backgroundSize = "110px 110px";
+    accueil.style.backgroundRepeat = `repeat`;
+    accueil.style.backgroundColor = "#C8D3A7";
+    accueil.style.backgroundPosition = "0 0, 55px 55px";
+    accueil.style.color = "#db4d00";
+}
